0826adcf71ff5b729f942d3332fcd182b94c25b7
Lawybanx <awwalbankole@gmail.com>
2021-02-23 15:04:30 +0100

    heroku commit

--- config/db.js
+++ config/db.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+// require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI || process.env.MONGO_URI_LOCAL,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      }
+    );
 
     console.log(`MongoDB Connected:${conn.connection.host}`);
   } catch (err) {
