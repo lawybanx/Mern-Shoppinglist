df4a168ffef40abdc43839842c812ae7c6627f8b
Lawybanx <awwalbankole@gmail.com>
2021-02-23 21:53:41 +0100

    dotenv

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './config/config.env' });
 
 const connectDB = async () => {
   try {
